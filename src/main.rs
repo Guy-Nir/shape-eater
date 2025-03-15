9961a024cb06f0b3285a6b7b4d4e3a23a87a474a
zhowkmif <guynir1@proton.me>
2025-03-15 17:26:34 +0200

    I hate ui

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ struct Ball;
 struct Wall;
 
 #[derive(Component)]
+struct InGameEntity;
+
+#[derive(Component)]
+struct DeathScreenEntity;
+
+#[derive(Component)]
 struct Numbered(i32);
 
 #[derive(Event)]
@@ -26,6 +32,24 @@ struct WallBounceStopwatch(Stopwatch);
 
 #[derive(Component)]
 struct PlayerText;
+
+#[derive(Resource)]
+struct HighScore(i32);
+
+#[derive(Resource)]
+struct CurrentScore(i32);
+
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct InGameSet;
+
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct DeathScreenSet;
+
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+enum GameState {
+    DeathScreen,
+    InGame,
+}
 
 const STARTING_NUMBER: i32 = 15;
 const SIZE_FACTOR: f32 = 1.5;
@@ -88,6 +112,15 @@ fn main() {
             }),
             PhysicsPlugins::default(),
         ))
+        .insert_state(GameState::InGame)
+        .configure_sets(Update, InGameSet.run_if(in_state(GameState::InGame)))
+        .configure_sets(
+            Update,
+            DeathScreenSet.run_if(in_state(GameState::DeathScreen)),
+        )
+        .insert_resource(Gravity(Vec2::NEG_Y * 1000.))
+        .insert_resource(HighScore(0))
+        .insert_resource(CurrentScore(0))
         .insert_resource(Gravity(Vec2::NEG_Y * 1000.))
         .insert_resource(BallSpawnTimer(Timer::from_seconds(
             0.5,
@@ -96,7 +129,11 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(WallBounceStopwatch(Stopwatch::new()))
         .add_event::<MovementAction>()
-        .add_systems(Startup, setup)
+        .add_systems(OnEnter(GameState::InGame), setup_game)
+        .add_systems(OnExit(GameState::InGame), handle_game_over)
+        .add_systems(OnEnter(GameState::DeathScreen), setup_death_screen)
+        .add_systems(OnExit(GameState::DeathScreen), death_screen_exit)
+        .add_systems(Update, restart_game.in_set(DeathScreenSet))
         .add_systems(
             Update,
             (
@@ -107,18 +144,18 @@ fn main() {
                 spawn_ball,
                 despawn_out_of_bounds_balls,
                 handle_hits,
-            ),
+            )
+                .in_set(InGameSet),
         )
         .run();
 }
 
-fn setup(
+fn setup_game(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2d);
-    commands.spawn((RigidBody::Dynamic, Collider::circle(0.5)));
 
     let walls = [
         ((1880., 20.), (0., Bound::UpperBound.value())),
@@ -130,6 +167,7 @@ fn setup(
     for (size, transform) in walls {
         commands.spawn((
             Wall,
+            InGameEntity,
             Sprite {
                 color: Color::srgb(0.0, 0.4, 0.7),
                 custom_size: Some(Vec2::new(size.0, size.1)),
@@ -157,12 +195,14 @@ fn setup(
                 ..default()
             },
             Transform::from_xyz(transform.0, transform.1, 99.),
+            InGameEntity,
         ));
     }
 
     commands
         .spawn((
             Player,
+            InGameEntity,
             CollidingEntities::default(),
             Numbered(STARTING_NUMBER),
             Mesh2d(meshes.add(Rectangle::new(
@@ -188,6 +228,105 @@ fn setup(
                 },
             ));
         });
+}
+
+fn handle_game_over(
+    mut commands: Commands,
+    in_game_entities: Query<Entity, With<InGameEntity>>,
+    asset_server: Res<AssetServer>,
+    player_score_query: Query<&Numbered, With<Player>>,
+    mut current_score: ResMut<CurrentScore>,
+) {
+    let Numbered(player_score) = player_score_query.single();
+    current_score.0 = *player_score;
+
+    commands.spawn(AudioPlayer::new(asset_server.load("sounds/game_over.ogg")));
+    for entity in in_game_entities.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn death_screen_exit(
+    mut commands: Commands,
+    death_screen_entities: Query<Entity, With<DeathScreenEntity>>,
+) {
+    for entity in death_screen_entities.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn setup_death_screen(
+    mut commands: Commands,
+    current_score: ResMut<CurrentScore>,
+    mut high_score: ResMut<HighScore>,
+) {
+    commands.spawn((
+        Text::new("Game over"),
+        TextFont {
+            font_size: 100.,
+            ..default()
+        },
+        DeathScreenEntity,
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(300.),
+            left: Val::Px(500.),
+            ..default()
+        },
+    ));
+
+    let high_score_text = if current_score.0 > high_score.0 {
+        high_score.0 = current_score.0;
+        "new high score!".to_string()
+    } else {
+        format!("high score - {}", high_score.0)
+    };
+
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        })
+        .with_children(|builder| {
+            builder
+                .spawn((
+                    Node {
+                        flex_direction: FlexDirection::Row,
+                        ..default()
+                    },
+                    DeathScreenEntity,
+                ))
+                .with_children(|builder| {
+                    builder.spawn((
+                        Text::new(format!("score - {}", current_score.0)),
+                        TextFont {
+                            font_size: 30.,
+                            ..default()
+                        },
+                        DeathScreenEntity,
+                    ));
+                    builder.spawn((
+                        Text::new(high_score_text),
+                        TextFont {
+                            font_size: 30.,
+                            ..default()
+                        },
+                        DeathScreenEntity,
+                    ));
+                });
+        });
+}
+
+fn restart_game(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut next_game_state: ResMut<NextState<GameState>>,
+) {
+    if keys.just_pressed(KeyCode::KeyR) {
+        next_game_state.set(GameState::InGame);
+    }
 }
 
 fn tick_stopwatch(mut stopwatch: ResMut<WallBounceStopwatch>, time: Res<Time>) {
@@ -241,6 +380,7 @@ fn spawn_ball(
         .spawn((
             Numbered(number),
             Ball,
+            InGameEntity,
             Mesh2d(meshes.add(Circle::new(number as f32 * SIZE_FACTOR / 2.))),
             MeshMaterial2d(materials.add(Color::srgb(1., 0., 0.))),
             Transform::from_translation(starting_point.extend(0.)),
@@ -277,18 +417,19 @@ fn handle_hits(
     mut meshes: ResMut<Assets<Mesh>>,
     asset_server: Res<AssetServer>,
     mut wall_bounce_stopwatch: ResMut<WallBounceStopwatch>,
+    mut next_game_state: ResMut<NextState<GameState>>,
 ) {
     for (player_velocity, hits, mut player_number, mut player_collider, mut player_mesh) in
         player_query.iter_mut()
     {
         for hit_entity in hits.iter() {
             if let Ok(Numbered(ball_number)) = ball_query.get(*hit_entity) {
-                let player_number_change = if *ball_number > player_number.0 {
-                    -player_number.0 + 1
-                } else {
-                    commands.spawn(AudioPlayer::new(asset_server.load("sounds/ball_eaten.ogg")));
-                    (*ball_number as f32 / 5.).ceil() as i32
-                };
+                if *ball_number > player_number.0 {
+                    next_game_state.set(GameState::DeathScreen);
+                    return;
+                }
+
+                let player_number_change = (*ball_number as f32 / 5.).ceil() as i32;
 
                 commands.entity(*hit_entity).despawn_recursive();
                 player_number.0 += player_number_change;
@@ -300,9 +441,9 @@ fn handle_hits(
                 let (mut child_text, mut child_text_font) = text_query.single_mut();
                 child_text.0 = player_number.0.to_string();
                 child_text_font.font_size = player_number.0 as f32 * FONT_SIZE_FACTOR;
+                commands.spawn(AudioPlayer::new(asset_server.load("sounds/ball_eaten.ogg")));
             } else if let Ok(_) = wall_query.get(*hit_entity) {
-                if player_velocity.length() > 50.
-                    && wall_bounce_stopwatch.0.elapsed_secs_f64() > 0.5
+                if player_velocity.y.abs() > 20. && wall_bounce_stopwatch.0.elapsed_secs_f64() > 0.1
                 {
                     wall_bounce_stopwatch.0.reset();
                     commands.spawn(AudioPlayer::new(
